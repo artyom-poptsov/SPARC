e8caf04dd4738174bbe398d3ff00bf95a250f826
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2025-08-23 14:29:46 +0300

    examples/music/daisy_bell: Add to the repository

---
+++ examples/music/daisy_bell/music.h
@@ -0,0 +1,43 @@
+#ifndef __MUSIC_H__
+#define __MUSIC_H__
+
+typedef struct {
+  // Количество нот в мелодии.
+  int length;
+  
+  // Индекс (номер) ноты.
+  int  note_index;
+
+  // Время начала воспроизведения ноты.
+  long note_start_time;
+
+  // Длина ноты.
+  long note_length;
+
+  // Задержка после ноты.
+  long note_delay;
+
+  // Текущее время.
+  long current_time;
+
+  // Время, прошедшее с начала воспроизведения
+  // ноты.
+  long time_diff;
+
+  // Виртуальный регистр OCR.
+  long ocr;
+
+  // Счётчик для таймера.
+  long counter;
+
+  // Цифровой порт для вывода звука.
+  byte speaker_pin;
+
+  // Текущее значение на выходном цифровом порту.
+  byte speaker_pin_value;
+} track_t;
+
+extern float daisy1[][2];
+extern const int LENGTH;
+
+#endif /* ifndef __MUSIC_H__ */
